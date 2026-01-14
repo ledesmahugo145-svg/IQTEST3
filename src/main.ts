b121767732b7f850a9ca783617ebb74dc90ad98f
ledesmahugo145-svg <ledesmahugo145@gmail.com>
2026-01-14 15:01:18 +0800

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import 'zone.js';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideExperimentalZonelessChangeDetection()
+  ]
+}).catch((err) => {
+  console.error('Bootstrap Error:', err);
+});
